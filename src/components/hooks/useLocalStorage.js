c7ab11ce5437dc73d05bc25047f88e663649111d
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-04-08 14:59:35 -0600

    useLocalStorage hook completed

---
+++ src/components/hooks/useLocalStorage.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+
+const useLocalStorage = (key, initialValue) => {
+    const { localStorage } = window
+
+    //getter method
+    const [storedValue, setStoredValue] = useState(() => {
+        const currentValue = localStorage.getItem(key)
+        return currentValue ? JSON.parse(currentValue) : initialValue
+    })
+    //setter method
+    const setValue = (value) => {
+        setStoredValue(value)
+        localStorage.setItem(key, JSON.stringify(value))
+    }
+    return [storedValue, setValue]
+}
+
+export default useLocalStorage
